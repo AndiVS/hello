150b5000f1aadd05b34e12fbecbeaa7c8a371f09
Andrei Saldyun <andrei.saldyun@effective-soft.com>
2022-02-28 18:56:19 +0300

    Initial commit

---
+++ api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello world ")
+	})
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":8080")))
+}
